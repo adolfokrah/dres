714f2b1487c6acde5225a1aa416e70bf4b057f1e
adolfokrah <adolfokrah@gmail.com>
2025-12-21 17:02:05 +0100

    fix: removed dres-web

--- dres-web/src/collections/VariantTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import { CollectionOverride } from '@payloadcms/plugin-ecommerce/types'
-
-export const VariantTypesCollection: CollectionOverride = ({ defaultCollection }) => ({
-  ...defaultCollection,
-  admin: {
-    ...defaultCollection.admin,
-    group: 'Products',
-  },
-})
