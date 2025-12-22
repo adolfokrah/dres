b7b149de0177ca9d714465fa6cf60a7a083a4afc
adolfokrah <adolfokrah@gmail.com>
2025-12-22 01:16:58 +0100

    Regenerate fresh migration with complete schema for empty production DB

--- dres-web/src/migrations/index.ts
+++ dres-web/src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20251221_233551 from './20251221_233551';
-import * as migration_20251221_235557 from './20251221_235557';
+import * as migration_20251222_001315 from './20251222_001315';
 
 export const migrations = [
   {
-    up: migration_20251221_233551.up,
-    down: migration_20251221_233551.down,
-    name: '20251221_233551',
-  },
-  {
-    up: migration_20251221_235557.up,
-    down: migration_20251221_235557.down,
-    name: '20251221_235557'
+    up: migration_20251222_001315.up,
+    down: migration_20251222_001315.down,
+    name: '20251222_001315'
   },
 ];
