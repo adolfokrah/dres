2c9ab7e4f91bf0c0f60b354952ab54772a462496
adolfokrah <adolfokrah@gmail.com>
2026-01-07 16:21:05 +0100

    fix; udpates

--- dres-web/next.config.js
+++ dres-web/next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL, 'http://dres.app', /* 'https://example.com' */].map((item) => {
         const url = new URL(item)
 
         return {
